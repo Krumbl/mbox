a803a369eefa55d623de381661c2637ac537544a
krumbl <krumbl77@gmail.com>
2020-06-06 19:26:59 -0500

    default box settings

---
+++ account/default/box/SavedVariables/Plater.lua
@@ -0,0 +1,16 @@
+
+PlaterDBChr = {
+	["buffsBanned"] = {
+	},
+	["first_run3"] = {
+		["Player-76-0A612DE2"] = true,
+	},
+	["spellRangeCheck"] = {
+		[73] = "Taunt",
+		[71] = "Taunt",
+		[72] = "Taunt",
+	},
+	["debuffsBanned"] = {
+	},
+	["resources_on_target"] = false,
+}
