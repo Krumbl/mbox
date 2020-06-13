8184b9754c9fcaf31af78b1d981966437f730887
krumbl <krumbl77@gmail.com>
2020-06-12 21:07:01 -0500

    automating healing rotation adding grumbl healer

---
+++ tmp/keeheal/Plater.lua
@@ -0,0 +1,16 @@
+
+PlaterDBChr = {
+	["buffsBanned"] = {
+	},
+	["first_run3"] = {
+		["Player-76-0A612E98"] = true,
+	},
+	["spellRangeCheck"] = {
+		[257] = "Smite",
+		[258] = "Mind Blast",
+		[256] = "Smite",
+	},
+	["debuffsBanned"] = {
+	},
+	["resources_on_target"] = false,
+}
